8025890232880b3a13d82682a9cd229da90cf720
shivampradhan <pradhanshivam@seasia.in>
2023-11-07 14:37:15 +0530

    'PORTFOLIO'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { ContactMeComponent } from './feature/contact-me/contact-me.component';
+import { AboutMeComponent } from './feature/about-me/about-me.component';
+import { ProjectsComponent } from './feature/projects/projects.component';
+import { SkillsComponent } from './feature/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path : 'Home', component : HomeComponent}, {path : "", component : HomeComponent}, {path : "Contact-me", component : ContactMeComponent}, {path : "About-me", component : AboutMeComponent}, {path : "Project", component : ProjectsComponent}, {path : "Skills", component : SkillsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './feature/home/home.component';
+import { AboutMeComponent } from './feature/about-me/about-me.component';
+import { ProjectsComponent } from './feature/projects/projects.component';
+import { ContactMeComponent } from './feature/contact-me/contact-me.component';
+import { SkillsComponent } from './feature/skills/skills.component';
+import { NavbarComponent } from './feature/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactMeComponent,
+    SkillsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
